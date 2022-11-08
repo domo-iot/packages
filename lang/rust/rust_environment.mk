dc509e30d81730185988c5015364c4d1572b8142
Luca Barbato <lu_zero@gentoo.org>
2022-11-08 09:42:57 +0100

    rust-lang: Bump rust version to 1.65
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- lang/rust/rust_environment.mk
+++ lang/rust/rust_environment.mk
@@ -21,21 +21,3 @@ ifeq ($(ARCH),"arm")
     RUSTC_TARGET_ARCH:=$(subst muslgnueabi,muslgnueabihf,$(RUSTC_TARGET_ARCH))
   endif
 endif
-
-
-# These allow Cargo packaged projects to be compile via $(call xxx/Compile/Cargo)
-define Host/Compile/Cargo
-	mkdir -p $(PKG_BUILD_DIR)/.cargo
-	cd $(PKG_BUILD_DIR) && \
-	  CARGO_HOME=$(CARGO_HOME) cargo update && \
-	  CARGO_HOME=$(CARGO_HOME) cargo build -v --release \
-	  --target $(RUSTC_TARGET_ARCH),$(RUST_HOST_ARCH)
-endef
-
-define Build/Compile/Cargo
-	mkdir -p $(PKG_BUILD_DIR)/.cargo
-	cd $(PKG_BUILD_DIR) && \
-	  CARGO_HOME=$(CARGO_HOME) cargo update && \
-	  echo -e "[target.$(RUSTC_TARGET_ARCH)]\nlinker = \"$(TARGET_CC_NOCACHE)\"" > .cargo/config && \
-	  CARGO_HOME=$(CARGO_HOME) CFLAGS=-mno-outline-atomics TARGET_CC=$(TARGET_CC_NOCACHE) CC=cc cargo build -v --release --target $(RUSTC_TARGET_ARCH)
-endef
