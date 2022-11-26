c007baeeb0554b54665b50c9641920cd0213b1b2
Luca Barbato <lu_zero@gentoo.org>
2022-11-26 17:17:55 +0100

    rust-lang: Simplify the build process
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- lang/rust/rust_build.mk
+++ lang/rust/rust_build.mk
@@ -4,14 +4,17 @@ define Host/Compile/Cargo
 	cd $(PKG_BUILD_DIR) && \
 	  CARGO_HOME=$(CARGO_HOME) cargo update && \
 	  CARGO_HOME=$(CARGO_HOME) cargo build -v --release \
-	  --target $(RUSTC_TARGET_ARCH),$(RUST_HOST_ARCH)
+	  --target $(RUST_HOST_ARCH)
 endef
 
-# TODO not hardcode the dynamic-linker
+# $(1) path to the package dir
+# $(2) additional arguments to cargo install
 define Build/Compile/Cargo
         mkdir -p $(PKG_BUILD_DIR)/.cargo
 	cd $(PKG_BUILD_DIR) && \
 	    CARGO_HOME=$(CARGO_HOME) cargo update && \
 	    echo -e "[target.$(RUSTC_TARGET_ARCH)]\nlinker = \"$(TARGET_CC_NOCACHE)\"\nrustflags = [\"-Ctarget-feature=-crt-static\"]" > .cargo/config && \
-	    CARGO_HOME=$(CARGO_HOME) CFLAGS=-mno-outline-atomics TARGET_CC=$(TARGET_CC_NOCACHE) CC=cc cargo build -v --release --target $(RUSTC_TARGET_ARCH)
+	    echo -e "\n[profile.stripped]\ninherits = \"release\"\nopt-level = \"s\"\nstrip = true" >> .cargo/config && \
+	    CARGO_HOME=$(CARGO_HOME) CFLAGS=-mno-outline-atomics TARGET_CC=$(TARGET_CC_NOCACHE) CC=cc \
+	    cargo install -v --profile stripped --target $(RUSTC_TARGET_ARCH) --root $(PKG_INSTALL_DIR) --path "$(if $(strip $(1)),$(strip $(1)),.)" $(2)
 endef
